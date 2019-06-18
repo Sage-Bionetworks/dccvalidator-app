994d589f7448a7c93e263ae00c857359843a7eb0
Kara Woo <kara.woo@sagebase.org>
2019-06-18 14:50:13 -0700

    Don't need so much space between reported results

--- global.R
+++ global.R
@@ -18,11 +18,9 @@ enableBookmarking(store = "url")
 #####################
 
 report_result <- function(result, emoji_prefix = NULL) {
-  div(
-    p(emo::ji(emoji_prefix), result$message),
-    br()
-  )
+  p(emo::ji(emoji_prefix), result$message)
 }
+
 report_results <- function(results, emoji_prefix = NULL) {
   map(results, report_result, emoji_prefix = emoji_prefix)
 }
